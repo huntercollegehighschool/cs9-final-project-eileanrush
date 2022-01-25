7fd73b2f9c1930ba31273f7bc3e084b0212f27a1
Eilean Faltin <94643871+eileanrush@users.noreply.github.com>
2022-01-25 04:05:07 +0000

    Copied my project into the main file.

--- main.py
+++ main.py
@@ -1,11 +1,107 @@
 """
-Name(s):
-Name of Project:
+Name(s): Eilean Faltin
+Name of Project: Madlibs
 """
 
-#Write the main part of your program here. Use of the other pages is optional.
+print("Welcome to MADLIBS!")
+print("\
+  ")
+import random
+story = random.randint(1, 4)
+if story == 1:
+  pluralnoun = input("Enter a plural noun: ")
+  person = input("Enter the name of a person: ")
+  noun = input("Enter a singular noun: ")
+  adjective = input("Enter an adjective: ")
+  noun1 = input("Enter another singular noun: ")
+  pluralnoun1 = input("Enter another plural noun: ")
+  noun2 = input("Enter another singular noun: ")
+  verb = input("Enter a verb: ")
+  pluralnoun2 = input("Enter another plural noun: ")
+  liquid = input("Enter a type of liquid: ")
+  adjective1 = input("Enter another adjective: ")
+  noun3 = input("Enter another singular noun: ")
+  verb1 = input("Enter another verb: ")
+  adjective2 = input("Enter another adjective: ")
+  liquid1 = input("Enter another type of liquid: ")
+  noun4 = input("Enter another singular noun: ")
+  print("\
+  ")
+  print("Story 1: Totally Tie-Dyed")
+  print("\
+  ") 
+  print("Back in the 60s, tie-dyed " + pluralnoun +  " became a popular fashion trend after " + person + " wore a psychadelic " + noun + " at a concert. Soon after, hippies donned this " + adjective + " apparel while promoting peace and " + noun1 + ". Today, anyone can create colorful " + pluralnoun1 + " in their own backyard. First, take a damp " + noun2 + ", " + verb + " and roll the fabric, then tie it with some tight rubber " + pluralnoun2 + ". Next, dip sections into vats of colored " + liquid + ". The longer it soaks, the more " + adjective1 + " the colors become! Finally, soak your creation in a mixture of " + noun3 + " and vinegar. This tangy solvent helps " + verb1 + " the dye, keeping colors looking " + adjective2 + " for years! And if tie-dye isn't your cup of " + liquid1 + ", it might make a cool gift for that special " + noun4 + "in your life!")
 
-#import page1  # uncomment if you're using page1
-#import page2  # uncomment if you're using page2
-#import page3  # uncomment if you're using page3
-#import page4  # uncomment if you're using page4
+
+if story == 2:
+  pluralnoun = input("Enter a plural noun: ")
+  adjective = input("Enter an adjective: ")
+  adjective1 = input("Enter another adjective: ")
+  pluralnoun1 = input("Enter another plural noun: ")
+  adjective2 = input("Enter another adjective: ")
+  bodypart = input("Enter a part of the body: ")
+  adverb = input("Enter an adverb: ")
+  verb = input("Enter a verb ending in -ing: ")
+  adjective3 = input("Enter another adjective: ")
+  adjective4 = input("Enter another adjective: ")
+  noun = input("Enter a singular noun: ")
+  bodypart1 = input("Enter another part of the body: ")
+  noun1 = input("Enter a another singular noun: ")
+  noun2 = input("Enter another singular noun: ")
+  noun3 = input("Enter another singular noun: ")
+  pluralnoun2 = input("Enter another plural noun: ")
+  adjective5 = input("Enter another adjective: ")
+  adjective6 = input("Enter another adjective: ")
+  bodypart2 = input("Enter another part of the body: ")
+  print("\
+  ")
+  print("Story 2: Heading Westward")
+  print("\
+  ") 
+  print("Good vacations are worth their weight in " + pluralnoun +  ". A/An " + adjective + " summer vacation for you and your " + adjective1 + " family is to visit the Rocky " + pluralnoun1 + " in Colorado. The first time you see these " + adjective2 + " mountains, your " + bodypart + " will thump " + adverb + ". If you're into camping, fishing, or " + verb + ", visit Arizona's Grand Canyon and enjoy a land of " + adjective3 + " landscapes and rich " + adjective4 + " history. Upon the sight of this magnificent, mile-deep, 1 and 1/2 billion-year-old " + noun + ", your " + bodypart1 + " will drop open and you won't be able to catch your " + noun1 + ". And then, there is the city of the Golden Gate " + noun2 + ", San Francisco, where you can spend the day watching a cable " + noun3 + " loaded with wide eyed " + pluralnoun2 + " or climb the city's " + adjective5 + " hills. A place made famous by Tony Bennett's rendition of 'I Left My " + bodypart2 + " In San Francisco'. So will you!")
+
+
+if story == 3:
+  print("\
+  ")
+  exclamation = input("Enter an exclamation: ")
+  sound = input("Enter a sound: ")
+  adjective = input("Enter an adjective: ")
+  noun = input("Enter a singular noun: ")
+  number = input("Enter a number: ")
+  verb = input("Enter a verb: ")
+  verb1 = input("Enter another verb: ")
+  verb2 = input("Enter another verb: ")
+  liquid = input("Enter a type of liquid: ")
+  number1 = input("Enter another number: ")
+  adjective1 = input("Enter another adjective: ")
+  number2 = input("Enter another number: ")
+  pluralnoun = input("Enter a plural noun: ")
+  pluralnoun1 = input("Enter another plural noun: ")
+  print("\
+  ")
+  print("Story 3: Crazy New Gadget")
+  print("\
+  ") 
+  print("There is a new toy on the market that has everyone saying " + exclamation + "! It is called the " + sound + " " + adjective + " " + noun + " box, and will be in stores in year " + number + ". The " + sound + " " + adjective + " " + noun + " box is a new gadget that lets you do just about anything! It " + verb + "s, " + verb1 + "s, it " + verb2 + "s, and it even serves " + liquid + "! It is easy to operate and requires no instructions! You can also have it custom made any size up to " + number1 + " inches at no extra charge! The original product is pocket-sized and " + adjective1 + ". There are " + number2 + " jacks on the product for 6V DC power, plus upgrades and add-ons. You can add head-phones, " + pluralnoun + ", monitors, " + pluralnoun1 + ", and more! Plus, it’s possible to use them all at the same time!")
+
+if story == 4:
+  print("\
+  ")
+  adjective = input("Enter an adjective: ")
+  noun = input("Enter a singular noun: ")
+  verb = input("Enter a verb in the past tense: ")
+  adverb = input("Enter an adverb: ")
+  adjective1 = input("Enter another adjective: ")
+  noun1 = input("Enter another singular noun: ")
+  adjective2 = input("Enter another adjective: ")
+  verb1 = input("Enter a verb: ")
+  adverb1 = input("Enter another adverb: ")
+  verb2 = input("Enter another verb in the past tense: ")
+  adjective3 = input("Enter another adjective: ")
+  print("\
+  ")
+  print("Story 3: Day at the Zoo")
+  print("\
+  ")
+  print("Today I went to the zoo. I saw a(n) " + adjective + " " + noun + " jumping up and down in its tree. It " + verb + " " + adverb + " through the large tunnel that led to its " + adjective1 + " " + noun + ". I got some peanuts and passed them through the cage to a gigantic gray " + noun1 + " towering above my head. Feeding that animal made me hungry. I went to get a " + adjective2 + " scoop of ice cream. It filled my stomach. Afterwards I had to " + verb1 + " " + adverb1 + " to catch our bus. When I got home I " + verb2 + " my mom for a " + adjective3 + " day at the zoo.")
